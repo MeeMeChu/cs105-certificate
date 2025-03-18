f79015919ece7a029bc1472640a4ea621af8685a
NYUBNOOB <tinritto@gmail.com>
2025-03-18 14:57:16 +0700

    event page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 import Footer from "@components/footer/footer";
 import Header from "@components/header/header";
 import { Container } from "@mui/material";
@@ -8,8 +7,16 @@ const HomePage = () => {
   return (
     <Fragment>
       <Header />
-      <Container>
-        HomePage
+      <Container
+        maxWidth="xl"
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        
       </Container>
       <Footer />
     </Fragment>
