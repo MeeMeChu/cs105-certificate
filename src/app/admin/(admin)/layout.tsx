8b6afc838a2ce3ae91a5adffd56028d47aabd111
MeeMeChu <tanakeet005@gmail.com>
2025-06-14 03:40:59 +0700

    🐛 FIX: delete public file

--- src/app/admin/(admin)/layout.tsx
+++ src/app/admin/(admin)/layout.tsx
@@ -1,8 +1,4 @@
-import { authOptions } from "@app/api/v1/auth/[...nextauth]/route";
 import LayoutAdmin from "@components/admin/layout/layout-admin";
-import { Role } from "@type/user";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
   // const session = await getServerSession(authOptions);
